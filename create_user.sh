7092130fa83d2ef911a683ac873a193624ba5e9c
Richard H <106378212+rh540640@users.noreply.github.com>
2023-08-30 12:31:16 +0530

    Delete create_user.sh

--- create_user.sh
+++
@@ -1,7 +0,0 @@
-# Create a new user on a system:
-#!/bin/bash
-username="john"
-password="password"
-sudo useradd $username
-echo "$username:$password" | sudo chpasswd
-echo "The user has been created."
