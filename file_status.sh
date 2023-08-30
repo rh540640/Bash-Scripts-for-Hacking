0a1446f8d68e491d12b75b30acb85e798af90e84
Richard H <106378212+rh540640@users.noreply.github.com>
2023-08-30 12:32:15 +0530

    Delete file_status.sh

--- file_status.sh
+++
@@ -1,9 +0,0 @@
-#Check if a file exists and if not, create it:
-#!/bin/bash
-filename="file.txt"
-if [ ! -f $filename ]; then
-    touch $filename
-    echo "The file has been created."
-else
-    echo "The file already exists."
-fi
