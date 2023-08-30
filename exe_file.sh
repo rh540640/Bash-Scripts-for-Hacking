5248821115825ff4772632d65f544f58d78d0121
Richard H <106378212+rh540640@users.noreply.github.com>
2023-08-30 12:31:26 +0530

    Delete exe_file.sh

--- exe_file.sh
+++
@@ -1,8 +0,0 @@
-#Check if a file is executable:
-#!/bin/bash
-filename="script.sh"
-if [ -x $filename ]; then
-    echo "The file $filename is executable."
-else
-    echo "The file $filename is not executable."
-fi
