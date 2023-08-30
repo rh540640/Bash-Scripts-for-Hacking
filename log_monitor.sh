5d0421c633c735ac7fef895cb184b79288e598e3
Richard H <106378212+rh540640@users.noreply.github.com>
2023-08-30 12:32:32 +0530

    Delete log_monitor.sh

--- log_monitor.sh
+++
@@ -1,8 +0,0 @@
-#Monitor a log file in real-time:
-#!/bin/bash
-logfile="/var/log/auth.log"
-tail -f $logfile | while read line; do
-    if echo $line | grep -q 'Failed password'; then
-        echo $line
-    fi
-done
